ebb3997fb6cbd92fb3100aba2d63a5dc980fc72b
Jose <jose.truyol@keela.com>
2024-08-12 18:25:14 +0000

    Sandbox snapshot commit

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ let profiles = [
     id: "2ad19172-9683-407d-9732-8397d58ddcb2",
     name: "Nick's Fundraising Profile",
     parentId: "78afca18-8162-4ed5-9a7b-212b98c9ec87",
+    total: 5000,
     currency: "CAD",
   },
 ];
