9380fad3e49941250e98d69585c072d239529c3f
Jose <jose.truyol@keela.com>
2024-08-12 21:01:01 +0000

    chore: final clean up

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ app.use(express.json());
  * Fundraising profiles have the following shape:
  * id - A unique identifier, UUID v4
  * name - The display name for the profile
- * total - The total amount raised, in cents
  * parentId - The id of the profile that this profile belongs to. An ID of null indicates that the profile belongs is the root campaign profile
  * currency - The currency the profile is tracking their total in
  */
@@ -23,7 +22,6 @@ let profiles = [
     id: "2ad19172-9683-407d-9732-8397d58ddcb2",
     name: "Nick's Fundraising Profile",
     parentId: "78afca18-8162-4ed5-9a7b-212b98c9ec87",
-    total: 5000,
     currency: "CAD",
   },
 ];
@@ -78,13 +76,6 @@ app.get("/profiles/:profile/donations", (req, res) => {
  * Submit a new donation to the profile with the given ID
  */
 app.post("/profiles/:profile/donations", (req, res) => {
-  // Your implementation here
-});
-
-/**
- * Submit a new donation to the campaign
- */
-app.post("/donations", (req, res) => {
   // Your implementation here
 });
 
